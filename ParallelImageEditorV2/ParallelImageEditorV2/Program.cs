dc56233b530a27ae5b27fb9df6f7d7bb78ef34ce
Monica-Emilia Varuicu <varuicu.monica.i6x@student.ucv.ro>
2024-05-20 20:06:01 +0300

    feat: use plinq

--- ParallelImageEditorV2/ParallelImageEditorV2/Program.cs
+++ ParallelImageEditorV2/ParallelImageEditorV2/Program.cs
@@ -16,7 +16,7 @@ namespace ParallelImageEditorV2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ParallelImageEditorV2());
         }
     }
 }
